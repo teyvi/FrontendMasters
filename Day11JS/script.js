0e65250fb394b9a5d62add56860db2e2378ada04
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-08 23:06:31 +0000

    updated HTML title and added map method implementation for transforming city names to uppercase

--- Day11JS/script.js
+++ Day11JS/script.js
@@ -3,10 +3,18 @@ const cities = [
     'Seattle', 'San Francisico','Salt lake City', 'Amsterdam','Honkong'
 ];
 
-//it takes each item in the object and iterate based on the function to be executed
+//it takes each item in the array and iterate based on the function to be executed
 
 //a function that logs each city and index
 cities.forEach(function(city,index)
 {
     console.log(city, index)
 })
+
+
+//using map on an array
+const newCities = cities.map(function (city){
+return city.toLocaleUpperCase()
+})
+console.log(newCities)
+
