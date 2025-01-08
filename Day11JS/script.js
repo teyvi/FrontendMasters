d913f1f9b5ab374b7426accb9028c1795be40902
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-08 23:26:45 +0000

    added sorting functionality for prime numbers and implemented reverse sorting; included string sorting for city names

--- Day11JS/script.js
+++ Day11JS/script.js
@@ -18,3 +18,30 @@ return city.toLocaleUpperCase()
 })
 console.log(newCities)
 
+
+//using sort Method but added another function because sorting works on strings 
+const primeNumbers= [10, 1, 2, 3, 5, 7, 11, 13,17];
+console.log(primeNumbers.sort(function (num1, num2) {
+    if (num1 > num2){
+        return 1;
+    }
+    else if(num2 > num1 ){
+        return -1;
+    }
+    return 0;
+}))
+
+//reverse sorting
+
+console.log(primeNumbers.sort(function (num1, num2) {
+    if (num1 > num2){
+        return -1;
+    }
+    else if(num2 > num1 ){
+        return 1;
+    }
+    return 0;
+}))
+
+//using the sort function on strings
+console.log(cities.sort())
