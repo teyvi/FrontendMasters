370cbaf7fb4e375610aeaea90aeb860a26bfb9fe
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-15 19:37:43 +0000

    created a function to handle the clear button

--- 5-github/end/calculator.js
+++ 5-github/end/calculator.js
@@ -2,43 +2,47 @@
 let runningTotal = 0;
 
 //print the initial value which is zero
-let buffer = "0"
+let buffer = "0";
 
 //hold previous operator
 let previousOperator;
 
-const screen = document.querySelector('.screen');
+const screen = document.querySelector(".screen");
 
 //function for the click of buttons
 
 function buttonClick(value) {
-if (isNaN(value) === 'number')
-{handleSymbol(value);}
-else{
-handleNumber(value);
-}
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
 }
 
 //function to hadle symbols
-function handleSymbol(symbol){}
-
-//function to handle numbers concatenate as string 
-function handleNumber(numberString){
-    if (buffer === '0'){
-        buffer = numberString;
-    } else{
-        buffer += numberString;
+function handleSymbol(symbol) {
+    if (symbol === 'C'){
+        buffer = '0';
+        runningTotal = 0;
     }
-    screen.innerText = buffer;
+}
+
+//function to handle numbers concatenate as string
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+  screen.innerText = buffer;
 }
 //this function get to called once the page is loaded
-function init (){
-    document.querySelector('.calc-buttons')
-    .addEventListener('click', function(event){
-        buttonClick(event.target.innerText);
-    })
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (event) {
+      buttonClick(event.target.innerText);
+    });
 }
 
 init();
-
-
