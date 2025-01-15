497f81ecc70327178c159c6b429dd09737f136ae
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-15 18:07:01 +0000

    add viewport meta tag for responsive design and initialize calculator JavaScript

---
+++ 5-github/end/calculator.js
@@ -0,0 +1,22 @@
+//hold numbers in memory
+let runningTotal = 0;
+
+//print the initial value which is zero
+let buffet = "0"
+
+//hold previous operator
+let previousOperator;
+
+const screen = document.querySelector('.screen');
+
+//function for the click of buttons
+
+function buttonClick() {}
+
+function init (){
+    
+}
+
+
+
+
