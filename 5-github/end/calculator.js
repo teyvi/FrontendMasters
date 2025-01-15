e9a43d0bb492cac1ab7d59fad485dcc25988d8a7
teyvi <44750503+teyvi@users.noreply.github.com>
2025-01-15 18:27:26 +0000

    correct buffer variable name and enhance buttonClick function to handle numbers and symbols

--- 5-github/end/calculator.js
+++ 5-github/end/calculator.js
@@ -2,7 +2,7 @@
 let runningTotal = 0;
 
 //print the initial value which is zero
-let buffet = "0"
+let buffer = "0"
 
 //hold previous operator
 let previousOperator;
@@ -11,16 +11,33 @@ const screen = document.querySelector('.screen');
 
 //function for the click of buttons
 
-function buttonClick(value) {}
+function buttonClick(value) {
+if (isNaN(value) === 'number')
+{handleSymbol(value);}
+else{
+handleNumber(value);
+}
+}
 
+//function to hadle symbols
+function handleSymbol(symbol){}
+
+//function to handle numbers concatenate as string 
+function handleNumber(numberString){
+    if (buffer === '0'){
+        buffer = numberString;
+    } else{
+        buffer = buffer + numberString;
+    }
+}
 //this function get to called once the page is loaded
 function init (){
     document.querySelector('.calc-buttons')
-    .addEventListerner('click', function(event){
+    .addEventListener('click', function(event){
         buttonClick(event.target.innerText);
     })
 }
 
-
+init();
 
 
